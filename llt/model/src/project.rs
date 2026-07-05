142b09bf28f127682aef5ccb0858176f648e58d6
slaveok <yarpetromack@gmail.com>
2026-07-05 17:26:05 +0300

    update model

--- llt/model/src/project.rs
+++ llt/model/src/project.rs
@@ -1,11 +1,18 @@
+use std::collections::HashMap;
+
 use crate::node::IR;
+
 
 pub struct Project {
     irs: Vec<IR>,
+    obfuscation_name: HashMap<String, bool>,
 }
 
 impl Project {
     pub fn new() -> Self {
-        Project { irs: Vec::new() }
+        Project { 
+            irs: Vec::new(),
+            obfuscation_name: HashMap::new() 
+        }
     }
 }
